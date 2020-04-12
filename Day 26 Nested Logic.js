862467fdd1e05f08d752b0234981c433cad2806a
Justine Robert <igunejustinerobert@gmail.com>
2020-04-12 22:52:36 +0300

    Day 26: Nested Logic - JavaScript Code

---
+++ Day 26 Nested Logic.js
@@ -0,0 +1,33 @@
+/*Day 26: Nested Logic
+https://www.hackerrank.com/challenges/30-nested-logic/problem*/
+function processData(input) {
+    //Enter your code here
+    var dates = input.split("\n");
+    var a = dates[0].split(" ").map(Number);
+    var b = dates[1].split(" ").map(Number);
+    var actDate = new Date(a[2], a[1], a[0]);
+    var expDate = new Date(b[2], b[1], b[0]);
+    var fine = 0;
+
+    if(actDate <= expDate)
+        fine = 0;
+    else if(a[1] == b[1] && a[2] == b[2])
+        fine = 15 * (a[0] - b[0]);
+    else if(a[2] == b[2])
+        fine = 500 * (a[1] - b[1]);
+    else
+        fine = 10000;
+    console.log(fine);
+
+} 
+//Code logic ends here
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
