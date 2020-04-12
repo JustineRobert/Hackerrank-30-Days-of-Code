6ed172fdce58c0d7ed3cd80aba57cea7192973e6
Justine Robert <igunejustinerobert@gmail.com>
2020-04-12 23:16:28 +0300

    Day 26: Nested Logic - Java Code

---
+++ Day 26 Nested Logic.java
@@ -0,0 +1,48 @@
+/*Day 26: Nested Logic
+https://www.hackerrank.com/challenges/30-nested-logic/problem*/
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+
+        int actualDay = sc.nextInt();
+        int actualMonth = sc.nextInt();
+        int actualYear = sc.nextInt();
+
+        int expectedDay = sc.nextInt();
+        int expectedMonth = sc.nextInt();
+        int expectedYear = sc.nextInt();
+
+
+        int fine = computeFine(actualDay, actualMonth, actualYear, expectedDay, expectedMonth, expectedYear);
+
+        System.out.println(fine);
+    }
+
+    static int computeFine(int actualDay, int actualMonth, int actualYear, int expectedDay, int expectedMonth, int expectedYear) {
+
+        int fine = 0;
+
+        if (actualYear > expectedYear) {
+            fine = 10000;
+        }
+        else if (actualYear == expectedYear) {
+
+            if (actualMonth > expectedMonth) {
+                fine = (actualMonth - expectedMonth) * 500;
+            }
+            else if (actualMonth == expectedMonth) {
+
+                if (actualDay > expectedDay) {
+                    fine = (actualDay - expectedDay) * 15;
+                }
+            }
+        }
+
+        return fine;
+    }
+}
